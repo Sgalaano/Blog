763bb8dd541a2f7dc7f29c88d2a47796c62cd6a4
sgalaano <sgalaano@gmail.com>
2019-06-03 11:08:02 +0300

    changes

--- instance/config.py
+++ instance/config.py
@@ -3,4 +3,4 @@ SECRET_KEY="Galaano1998"
 MAIL_USERNAME="sgalaano@gmail.com"
 MAIL_PASSWORD="galaano"
 
-DATABASE_URL='postgresql+psycopg2://galaano:@localhost/blog'
+DATABASE_URL='postgresql+psycopg2://galaano:galaano@localhost/blog'
--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ export SECRET_KEY=Galaano1998
 export MAIL_USERNAME=sgalaano@gmail.com
 export MAIL_PASSWORD=Galaano1998
 
-export DATABASE_URL=postgresql+psycopg2://galaano:@localhost/blog
+export DATABASE_URL=postgresql+psycopg2://galaano:galaano@localhost/blog
 
 python3.6 manage.py server
